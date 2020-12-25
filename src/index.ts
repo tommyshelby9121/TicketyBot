4731247b592e4184a4e92add387abe165880c628
Sycrixx <biometricmc@gmail.com>
2020-12-25 21:38:18 +1100

    Logged into client & create ready event

--- src/index.ts
+++ src/index.ts
@@ -8,3 +8,25 @@ const client:TicketyClient = new TicketyClient({
    token: config.token,
    prefix: config.prefix,
 });
+
+// Ready Event
+client.once("ready", () => {
+   console.log(`Logged in as ${client.user?.tag ?? ""}`);
+
+   try {
+       void client.user?.setPresence({
+           status: "dnd",
+           activity: {
+               name: config.presence_message,
+               type: config.presence_type
+           },
+       });
+   }
+   catch (err) {
+       console.error(`Could not set startup presence: ${err}`);
+       process.exit(1);
+   }
+});
+
+// Client Login
+client.login(client.config.token).catch(err => console.error(err));
