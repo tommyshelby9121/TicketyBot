75bbf34d5d4d734eb5fed215a71ff32204ce868a
Sycrixx <biometricmc@gmail.com>
2020-12-25 22:12:12 +1100

    Create config example

---
+++ config.example.js
@@ -0,0 +1,24 @@
+module.exports = {
+    // Discord Config
+    token: "",
+    prefix: "!",
+    presence_type: "",
+    presence_message: "",
+
+    // Support Role
+    support_role: "",
+
+    // Logging
+    log_command_executes: true,
+    ticket_log_channel: "",
+
+    // Creating Ticket
+    ticket_parent: "",
+
+    // DMing User
+    dm_user: true,
+
+    // Closing Ticket
+    confirm_ticket_close: true,
+    create_transcripts: true,
+};
