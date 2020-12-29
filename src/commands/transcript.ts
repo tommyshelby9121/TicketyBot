b3b2ad4e37cc7621e12f243aec6abcb7f6100759
Sycrixx <biometricmc@gmail.com>
2020-12-30 09:24:33 +1100

    Create transcript command

---
+++ src/commands/transcript.ts
@@ -0,0 +1,21 @@
+import { TicketyClient } from "../struct/Client";
+import {DMChannel, Message, MessageAttachment} from "discord.js";
+import {generateTranscript} from "../utils/generateTranscript";
+import {unlink} from "fs/promises";
+
+export default {
+    name: "transcript",
+    description: "Generates a transcript",
+    // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+    async execute(client:TicketyClient, message:Message) {
+        await generateTranscript(message, message.channel);
+        if (!(message.channel instanceof DMChannel)) {
+            const transcript = new MessageAttachment(`./src/transcripts/${message.channel.name}.html`);
+            await message.channel.send(transcript).then(() => {
+                if (!(message.channel instanceof DMChannel)) {
+                    unlink(`./src/transcripts/${message.channel.name}.html`);
+                }
+            }).catch(err => console.error(err));
+        }
+    }
+}
